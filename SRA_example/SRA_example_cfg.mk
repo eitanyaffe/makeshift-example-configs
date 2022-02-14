79711b8623157722b1e781e69b32e153593cecca
Eitan Yaffe <eitan.yaffe@gmail.com>
2022-02-14 14:24:33 -0800

    bucket names organized

--- SRA_example/SRA_example_cfg.mk
+++ SRA_example/SRA_example_cfg.mk
@@ -14,16 +14,16 @@ GCP_LOCATION?=us-west1
 # specify basic parameters
 ##########################################################################################
 
-BUCKET_VER?=v1
+BUCKET_VER?=v2
 
 # specify project name
-PROJECT_NAME=ms-sra-example-$(GCP_PROJECT_ID)
+PROJECT_NAME=sra-example-$(GCP_PROJECT_ID)
 
 # specify unique input bucket
-INPUT_BUCKET=gs://ms-sra-example-$(GCP_PROJECT_ID)-input-$(BUCKET_VER)
+INPUT_BUCKET=gs://ms-$(PROJECT_NAME)-input-$(BUCKET_VER)
 
 # specify unique work bucket
-WORK_BUCKET=gs://ms-sra-example-$(GCP_PROJECT_ID)-work-$(BUCKET_VER)
+WORK_BUCKET=gs://ms-$(PROJECT_NAME)-work-$(BUCKET_VER)
 
 ##########################################################################################
 ##########################################################################################
@@ -50,7 +50,7 @@ $(call _class_instance,gmount,IN,$(INPUT_BUCKET) standard INPUT_DIR)
 $(call _class_instance,gmount,OUT,$(WORK_BUCKET) standard OUTPUT_DIR)
 
 # pipeline code uploaded here
-$(call _class_instance,gmount,B,gs://relman-makeshift-bin standard BIN_DIR)
+$(call _class_instance,gmount,B,$(BINARY_BUCKET) standard BIN_DIR)
 
 # deconseq bucket (auxiliary bucket)
 $(call _class_instance,gmount,DE,gs://yaffe-deconseq standard DECONSEQ_BASE_DIR)
