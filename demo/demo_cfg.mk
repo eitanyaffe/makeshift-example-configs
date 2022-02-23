a80128524304ed082cebc0e006dc4e72c277f6ee
eitanyaffe <eitan.yaffe@gmail.com>
2022-02-22 18:13:16 -0800

    Update demo_cfg.mk

--- demo/demo_cfg.mk
+++ demo/demo_cfg.mk
@@ -8,9 +8,8 @@ PROJECT_NAME=demo-$(GCP_PROJECT_ID)
 # output bucket
 OUTPUT_BUCKET=gs://ms-$(PROJECT_NAME)-output
 
-# docker image
-# GCP_IMAGE_NAME=mdocker-base
-GCP_GCR_IMAGE_PATH=gcr.io/relman-yaffe/mdocker-base
+# docker image can be from any docker repository
+GCP_GCR_IMAGE_PATH=eitanyaffe/mdocker-base:v1.00
 
 ##########################################################################################
 # register buckets
